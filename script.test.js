0ba8341a80ddcaf18ecc54ca6c9fca5f6c9f1782
Tamara Simões <tamara.simoes@betrybe.com>
2022-04-04 23:12:37 -0300

    Adiciona describe para agrupar os testes

--- script.test.js
+++ script.test.js
@@ -1,29 +1,35 @@
-const {sum, summationOf} = require('./script');
+const { sum, summationOf } = require('./script');
 
-test('dois mais dois é quatro', () => {
+describe('A função sum', () => {
+  test('dois mais dois é quatro', () => {
     expect(sum(2, 2)).toBe(4);
   });
-
+  
   test('5.1 mais 6.55 é 11.65', () => {
     expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
   });
-
+  
   test('-5 mais -39 é -44', () => {
     expect(sum(-5, -39)).toBe(-44);
   });
+});
 
+describe('A função summationOf', () => {
   test('Espera que a função summationOF exista', () => {
     expect(typeof summationOf).toBe('function');
   });
-
+  
   test('Espera-se que o somatório de 1 seja igual a 1', () => {
     expect(summationOf(1)).toBe(1);
   });
-
+  
   test('Espera-se que o somatório de 3 seja igual a 6', () => {
     expect(summationOf(3)).toBe(6);
   });
-
+  
   test('Espera-se que o somatório de 5 seja igual a 15', () => {
     expect(summationOf(5)).toBe(15);
   });
+});
+
+
