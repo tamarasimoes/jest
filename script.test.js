0952f67166cf97564a91626e442ce40d71b9b2a6
Tamara Simões <tamara.simoes@betrybe.com>
2022-04-04 23:13:34 -0300

    Adiciona it

--- script.test.js
+++ script.test.js
@@ -1,15 +1,15 @@
 const { sum, summationOf } = require('./script');
 
 describe('A função sum', () => {
-  test('dois mais dois é quatro', () => {
+  it('dois mais dois é quatro', () => {
     expect(sum(2, 2)).toBe(4);
   });
   
-  test('5.1 mais 6.55 é 11.65', () => {
+  it('5.1 mais 6.55 é 11.65', () => {
     expect(sum(5.1, 6.55)).toBeCloseTo(11.65);
   });
   
-  test('-5 mais -39 é -44', () => {
+  it('-5 mais -39 é -44', () => {
     expect(sum(-5, -39)).toBe(-44);
   });
 });
